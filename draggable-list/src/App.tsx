a82867506d70a5ade2b6cc6076141b972186b650
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-12-18 19:26:14 +0900

    practice: draggable-list

--- draggable-list/src/App.tsx
+++ draggable-list/src/App.tsx
@@ -5,10 +5,66 @@ import clamp from 'lodash.clamp';
 import swap from 'lodash-move';
 import styles from './styles.module.css';
 
-// const fn=(order:number[]);
+const fn =
+  (order: number[], active = false, originalIndex = 0, curIndex = 0, y = 0) =>
+  (index: number) =>
+    active && index === originalIndex
+      ? {
+          y: curIndex * y, // 블럭 애니메이션 y축 이동 정도
+          scale: 1.1,
+          zIndex: 1,
+          shadow: 15,
+          immediate: (key: string) => key === 'zIndex',
+          config: (key: string) =>
+            key === 'y' ? config.stiff : config.default,
+        }
+      : {
+          y: order.indexOf(index) * 100,
+          scale: 1,
+          zIndex: 0,
+          shadow: 1,
+          immediate: false,
+        };
 
-function App() {
-  return <div className='App'></div>;
+function DraggableList({ items }: { items: string[] }) {
+  const order = useRef(items.map((_, index) => index)); //item order
+  const [springs, api] = useSprings(items.length, fn(order.current));
+  const bind = useDrag(({ args: [originalIndex], active, movement: [, y] }) => {
+    const curIndex = order.current.indexOf(originalIndex);
+    const curRow = clamp(
+      Math.round((curIndex * 2 + y) / 100),
+      0,
+      items.length - 1
+    );
+    const newOrder = swap(order.current, curIndex, curRow);
+    api.start(fn(newOrder, active, originalIndex, curIndex, y));
+    if (!active) order.current = newOrder;
+  });
+  return (
+    <div className={styles.content} style={{ height: items.length * 100 }}>
+      {springs.map(({ zIndex, shadow, y, scale }, i) => (
+        <animated.div
+          {...bind(i)}
+          key={i}
+          style={{
+            zIndex,
+            boxShadow: shadow.to(
+              (s) => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`
+            ),
+            y,
+            scale,
+          }}
+          children={items[i]}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <div className='flex fill center'>
+      <DraggableList items={'Lorem ipsum dolor sit'.split(' ')} />
+    </div>
+  );
+}
