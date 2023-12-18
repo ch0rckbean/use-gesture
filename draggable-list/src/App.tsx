99ff76046411a81e621529fd0349dbdc4d8d7edf
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-12-18 18:40:06 +0900

    chore: file setting

--- draggable-list/src/App.tsx
+++ draggable-list/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSprings, animated, config } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
+import clamp from 'lodash.clamp';
+import swap from 'lodash-move';
+import styles from './styles.module.css';
+
+// const fn=(order:number[]);
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
