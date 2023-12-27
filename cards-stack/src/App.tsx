74f7b5c240662c12745d35ccae9a568ce4d534f3
Bean <86273626+ch0rckbean@users.noreply.github.com>
2023-12-27 22:02:03 +0900

    study: cards-stack

--- cards-stack/src/App.tsx
+++ cards-stack/src/App.tsx
@@ -1,4 +1,88 @@
-import React from 'react';
+import { useState } from 'react';
+import { useSprings, animated, to as interpolate } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
+
+import styles from './styles.module.css';
+
+const cards = [
+  'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/3/3a/TheLovers.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg',
+];
+
+const to = (i: number) => ({
+  x: 0,
+  y: i * -4,
+  scale: 1,
+  rot: -10 + Math.random() * 20,
+  delat: i * 100,
+});
+const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+const trans = (r: number, s: number) =>
+  `perspective(1500px) rotateX(30deg) rotateY(${
+    r / 10
+  }deg) rotateZ(${r}deg) scale(${s})`;
+
+function Deck() {
+  const [gone] = useState(() => new Set()); // flick out된 모든 카드 set
+  const [props, api] = useSprings(cards.length, (i) => ({
+    ...to(i),
+    from: from(i),
+  }));
+
+  // Gesture
+  const bind = useDrag(
+    ({
+      args: [index],
+      active,
+      movement: [mx],
+      direction: [xDir],
+      velocity: [vx],
+    }) => {
+      const trigger = vx > 0.2; // flick 빠르게 하면 날아가는 효과 trigger
+      if (!active && trigger) gone.add(index);
+      api.start((i) => {
+        if (index !== i) return;
+        const isGone = gone.has(index);
+        const x = isGone ? (200 + window.innerWidth) * xDir : active ? mx : 0;
+        const rot = mx / 100 + (isGone ? xDir * 10 * vx : 0);
+        const scale = active ? 1.1 : 1;
+        return {
+          x,
+          rot,
+          scale,
+          delay: undefined,
+          config: { friction: 50, tension: active ? 800 : isGone ? 200 : 500 },
+        };
+      });
+
+      if (!active && gone.size === cards.length) {
+        setTimeout(() => {
+          gone.clear();
+          api.start((i) => to(i));
+        }, 600);
+      }
+    }
+  );
+  return (
+    <>
+      {props.map(({ x, y, rot, scale }, i) => (
+        <animated.div className={styles.deck} key={i} style={{ x, y }}>
+          <animated.div
+            {...bind(i)}
+            style={{
+              transform: interpolate([rot, scale], trans),
+              backgroundImage: `url(${cards[i]})`,
+            }}
+          />
+        </animated.div>
+      ))}
+    </>
+  );
+}
 
 function App() {
   return <div className='App'></div>;
